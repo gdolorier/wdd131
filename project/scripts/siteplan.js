255056d2c596d4fba23f7e68ccf02536785e4505
gdolorier <gdolorier.p@gmail.com>
2024-12-19 18:40:40 -0500

    siteplan

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+  
+    const lastModified = document.lastModified;
+    document.getElementById(
+      "lastModified"
+    ).textContent = `Last Modified: ${lastModified}`;
+  });
